0d431fac725c454673fae7bdbd6c45f9d8abd617
Dananjana Wijesooriya <dananjanawijesooriya@gmail.com>
2026-05-25 08:03:47 +0530

    Added tests for calculator methods

--- calculatorTest.java
+++ calculatorTest.java
@@ -1,7 +1,27 @@
 public class CalculatorTest {
     public static void main(String[] args) {
+
         Calculator cal=new Calculator();
+
         if(cal.add(2,3)==5){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+        if(cal.substract(10,5)==5){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+        if(cal.multiply(4,5)==20){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+        if(cal.divide(20,4)==5){
             System.out.println("Test Passed");
         }else{
             System.out.println("Test Failed");
